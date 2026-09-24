3d95da4d02d29b7208bf8d433a16feb8a704a2d4
youssefbenregaya-rgb <youssef.benregaya@edu.isetcom.tn>
2026-09-24 14:21:42 +0100

    Create Testenv.java

---
+++ Testenv.java
@@ -0,0 +1,7 @@
+public class TestEnv {
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println(" ENVIRONNEMENT VISUAL STUDIO + JAVA PRÊT !");
+        System.out.println("========================================");
+    }
+}
